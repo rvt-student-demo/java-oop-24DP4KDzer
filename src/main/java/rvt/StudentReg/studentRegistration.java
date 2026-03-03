6b505f64f705737c217e0a837f4198a935f975ad
Kaspars Dzerkals <a242728kd@rvt.lv>
2026-03-03 10:10:20 +0200

    Pievienoju Student reg. 2.0

--- src/main/java/rvt/StudentReg/studentRegistration.java
+++ src/main/java/rvt/StudentReg/studentRegistration.java
@@ -1,20 +1,46 @@
 package rvt.StudentReg;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class studentRegistration {
     public static void main(String[] args){
-        System.out.printf("+-----------------+-----------------+---------------------+--------------------+%n");
-        System.out.printf("| %-15s | %-15s | %-19s | %-18s |\n", "Name", "Surname", "Email", "Phone");
-        System.out.printf("+-----------------+-----------------+---------------------+--------------------+%n");
-        String[] names = {"Janis, Petersons, Janitis@gmail.com, 371+ 27734151", "Gatis, Strazdins, Gatis@gmail.com, 371+ 63713121", "Davis, Vrecumnieks, Davitis@gmail.com, 371+ 12121111"};
-
-        for (int i = 0; i < names.length; i++){
-            String parts[] = names[i].split(",");
-            System.out.printf("| %-15s | %-15s | %-19s | %-18s |\n", parts[0], parts[1], parts[2], parts[3] );
+        Scanner scanner = new Scanner(System.in);
+        List<String> vardi = new ArrayList<>();
+        while (true){
+            System.out.print("Input Your\n'Name' 'Surname'\n'Email' 'Phone'\nSeparated by ','\n>>> ");
+            String input = scanner.nextLine();
+            if (input.equals("stop")){
+                System.out.println("You have stopped adding names");
+                break;
+            }
+            System.out.println("Your Data Has Been Added \n(Type 'stop' if you want to stop adding names)");
+            vardi.add(input);
         }
 
-        System.out.printf("+-----------------+-----------------+---------------------+--------------------+%n");
+        vardi.add("Janis, Vercumnieks, janisjanis@gmail.com, 27272727");
+        vardi.add("Gatis, Strazdins, gstrazdins@gmail.com, 67232121");
+        
+
+        System.out.printf("+-----------------+-----------------+------------------------------+---------------------+%n");
+        System.out.printf("| %-15s | %-15s | %-30s | %-17s |\n", "Name", "Surname", "Email", "Phone");
+        System.out.printf("+-----------------+-----------------+------------------------------+---------------------+%n");
+        
+
+       for (String student : vardi){
+        String[] parts = student.split(",");
+
+        if (parts.length == 4){
+            System.out.printf("| %-15s | %-15s | %-30s | %-17s |\n",
+                        parts[0].trim(),
+                        parts[1].trim(),
+                        parts[2].trim(),
+                        parts[3].trim());
+        }
+       }
+
+        System.out.printf("+-----------------+-----------------+------------------------------+---------------------+%n");
         
     }
     
